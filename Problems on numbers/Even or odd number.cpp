93f1d6661b8c7e83dbc89357f064cd515a662841
Dipanwita31roy <98378731+Dipanwita31roy@users.noreply.github.com>
2022-02-03 17:19:13 +0530

    Create Even or odd number.cpp

---
+++ Problems on numbers/Even or odd number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int p;
+ cin>>p;
+ if(p%2==0)
+ {
+  cout<<" This is a even number";
+ }
+ else 
+ {
+  cout<<" This a odd number";
+  }
+  return 0;
+}
+  
+
